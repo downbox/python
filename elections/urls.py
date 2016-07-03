f9d2a277f2dbec9e2083e4a66c72f6f348cf961e
Jang Hyeseong <freekind@naver.com>
2016-07-03 17:48:06 +0900

    커밋
    커밋

---
+++ elections/urls.py
@@ -0,0 +1,7 @@
+#C\Code\mysite\elections\urls.py
+from django.conf.urls import url
+from . import views #.은 현재 폴더(elections)를 의미합니다.
+
+urlpatterns = [
+    url(r'^$', views.index), #위의 urls.py와는 달리 include가 없습니다.
+]
---
+++ elections/views.py
@@ -0,0 +1,6 @@
+#C\Code\mysite\elections\views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world")
