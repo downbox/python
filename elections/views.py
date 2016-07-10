bb14ab476697865da452d3a4ac79595e2b7c90dd
conquerex <conquerex@gmail.com>
2016-07-10 21:39:00 +0900

    2줄 만들기
    2줄 만들기

--- elections/views.py
+++ elections/views.py
@@ -10,17 +10,17 @@ def index(request):
 
     result = pey.sum(1,1)
 
-    print("=== ",cal1.adder(3))
-    print(cal1.adder(4))
-    print(cal2.adder(3))
-    result2 = cal2.adder(11)
-    print(result2)
-    print(type(result))
+    #print("=== ",cal1.adder(3))
+    #print(cal1.adder(4))
+    #print(cal2.adder(3))
+    #result2 = cal2.adder(11)
+    #print(result2)
+    #print(type(result))
 
     # 상속 예시
-    pey = HousePark("응용")
-    juliet = HouseKim("줄리엣")
-    pey.love(juliet)
-    pey + juliet
+    pey = HousePark('응용')
+    juliet = HouseKim('줄리엣')
+    result = pey.love(juliet)
+    result1 = pey + juliet
 
-    return HttpResponse(result)
+    return HttpResponse(result + '<br>' + result1)
