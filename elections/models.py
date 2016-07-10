060f420f3e7127ea81f5f23857ff75f302f112dd
conquerex <conquerex@gmail.com>
2016-07-10 19:51:55 +0900

    장고로 클래서 메세지 출력

--- elections/models.py
+++ elections/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.http import HttpResponse
 
 class Calculator:
     def __init__(self):
@@ -7,3 +8,12 @@ class Calculator:
     def adder(self, num):
         self.result += num
         return self.result
+
+class Service:
+    secret = "영구는 배꼽이 두 개다"
+    def __init__(self, name):
+        self.name = name
+    def sum(self, a, b):
+        result = a + b
+        message = "%s님 %s + %s = %s입니다." % (self.name, a, b, result)
+        return message
--- elections/views.py
+++ elections/views.py
@@ -1,15 +1,20 @@
 #C\Code\mysite\elections\views.py
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Calculator
+from .models import *
 
 def index(request):
     cal1 = Calculator()
     cal2 = Calculator()
+    pey = Service('영구')
 
-    print(cal1.adder(3))
+    result = pey.sum(1,1)
+
+    print("=== ",cal1.adder(3))
     print(cal1.adder(4))
     print(cal2.adder(3))
-    print(cal2.adder(11))
+    result2 = cal2.adder(11)
+    print(result2)
+    print(type(result))
 
-    return HttpResponse(cal2.adder(11))
+    return HttpResponse(result)
