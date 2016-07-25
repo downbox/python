0af21d1a6815b9fca01db2a4aa6331cd730f6b6c
conquerex <conquerex@gmail.com>
2016-07-25 23:37:22 +0900

    git test용

--- study_case/mod2.py
+++ study_case/mod2.py
@@ -1,14 +1,17 @@
 PI = 3.141592
 
-class Math: 
-    def solv(self, r): 
-        return PI * (r ** 2) 
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
 
-def sum(a, b): 
-    return a+b 
+def sum(a, b):
+    return a+b
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     print(PI)
-    a = Math() 
-    print(a.solv(2)) 
+    a = Math()
+    print(a.solv(2))
     print(sum(PI , 4.4))
+
+
+# git test용도
